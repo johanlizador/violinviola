0eb200518c1a5ff7502d053511c9f474473e09bf
johanlizador <74465790+johanlizador@users.noreply.github.com>
2026-09-18 12:28:25 -0400

    Call updateScoreUI during system initialization
    Added updateScoreUI function call to the initialization process.

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function initSystem() {
 
   updateRecordingUI();
   updateShareButton();
+  updateScoreUI();
 
   // Llenar listas de audio si ya hay permisos concedidos previamente
   populateAudioOutputs();
